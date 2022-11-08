699dda0c86e4d810344aaf791e9984caa02532e5
Zyad <>
2022-11-08 17:19:43 +0000

    Beautifying UX

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :created_events, :foreign_key => 'creator_id', :class_name => 'Event'
-  has_many :event_users
-  has_many :attended_events, through: :event_users, source: :event
+  has_many :created_events, :foreign_key => 'creator_id', :class_name => 'Event', dependent: :delete_all
+  has_many :event_users, dependent: :delete_all
+  has_many :attended_events, through: :event_users, source: :event, dependent: :delete_all
 
   validates :name, presence: true, length: { minimum: 2 }
 
